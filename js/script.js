63b29ca13a947c9e8a9be0b7ef02a247634cfa53
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-25 15:29:17 +0200

    create if/else (%3-%5)

--- js/script.js
+++ js/script.js
@@ -2,4 +2,11 @@ console.log("Giovedì");
 
 for (let i = 1; i < 101; i++) {
   console.log(i);
+
+  if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else {
+  }
 }
