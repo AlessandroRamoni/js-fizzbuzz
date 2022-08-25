ca0a5eee287dc1a7df71e3e6d09c01f79045c33a
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-25 17:38:53 +0200

    working on .innerHTML

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 console.log("Giovedì");
 
+/*
 for (let i = 1; i < 101; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("BuffFuzz");
@@ -10,17 +11,25 @@ for (let i = 1; i < 101; i++) {
   } else {
     console.log(i);
   }
-
-  const griglia = document.querySelector(".container");
-  for (let i = 1; i < 101; i++) {
-    const quadrato = document.createElement("div");
-    griglia.append(quadrato);
-    quadrato.innerHTML = i;
-    quadrato.style.textAlign = "center";
-  }
-}
-
-console.log(i);
-/*
-
 */
+
+const griglia = document.querySelector(".container");
+const quadrato = document.createElement("div");
+
+griglia.append(quadrato);
+quadrato.style.textAlign = "center";
+for (let i = 1; i < 101; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    quadrato.innerHTML = "BuzzFuzz";
+    console.log("BuffFuzz");
+  } else if (i % 3 === 0) {
+    quadrato.innerHTML = "fizz";
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    quadrato.innerHTML = "buzzzz";
+    console.log("buzz");
+  } else {
+    quadrato.innerHTML = i;
+  }
+  console.log(i);
+}
