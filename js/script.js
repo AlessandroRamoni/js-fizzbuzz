581491fca9d6754434e3c0e52bf911842f1d08b1
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-26 09:35:40 +0200

    add classList.add()

--- js/script.js
+++ js/script.js
@@ -8,19 +8,23 @@ for (let i = 1; i < 101; i++) {
   griglia.append(quadrato);
   if (i % 3 === 0 && i % 5 === 0) {
     quadrato.innerHTML = "BuzzFuzz";
-    quadrato.style.backgroundColor = "#0cd6a1";
+    // quadrato.style.backgroundColor = "#0cd6a1";
+    quadrato.classList.add("backgreen");
     console.log("BuffFuzz");
   } else if (i % 3 === 0) {
     quadrato.innerHTML = "fizz";
-    quadrato.style.backgroundColor = "#ffd166";
+    // quadrato.style.backgroundColor = "#ffd166";
+    quadrato.classList.add("backpink");
     console.log("fizz");
   } else if (i % 5 === 0) {
     quadrato.innerHTML = "buzzzz";
-    quadrato.style.backgroundColor = "#f0466f";
+    // quadrato.style.backgroundColor = "#f0466f";
+    quadrato.classList.add("backblue");
     console.log("buzz");
   } else {
     quadrato.innerHTML = i;
-    quadrato.style.backgroundColor = "#1389b2";
+    // quadrato.style.backgroundColor = "#1389b2";
+    quadrato.classList.add("backyellow");
   }
 
   console.log(i);
@@ -29,9 +33,9 @@ for (let i = 1; i < 101; i++) {
     quadrato.style.backgroundColor = "red";
   });
 
-  //   quadrato.addEventListener("mouseleave", function () {
-  //     quadrato.style.backgroundColor = "";
-  //   });
+  quadrato.addEventListener("mouseleave", function () {
+    quadrato.style.backgroundColor = "initial";
+  });
 }
 
 /*
