27c7a95248b6990f74a31ca66b4876f1ccb079e8
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-27 15:57:33 +0200

    working on "click" event

--- js/script.js
+++ js/script.js
@@ -8,34 +8,39 @@ for (let i = 1; i <= 100; i++) {
   griglia.append(quadrato);
   if (i % 3 === 0 && i % 5 === 0) {
     quadrato.innerHTML = "BuzzFuzz";
-    // quadrato.style.backgroundColor = "#0cd6a1";
+
     quadrato.classList.add("backgreen");
     console.log("BuffFuzz");
   } else if (i % 3 === 0) {
     quadrato.innerHTML = "fizz";
-    // quadrato.style.backgroundColor = "#ffd166";
+
     quadrato.classList.add("backpink");
     console.log("fizz");
   } else if (i % 5 === 0) {
     quadrato.innerHTML = "buzzzz";
-    // quadrato.style.backgroundColor = "#f0466f";
+
     quadrato.classList.add("backblue");
     console.log("buzz");
   } else {
     quadrato.innerHTML = i;
-    // quadrato.style.backgroundColor = "#1389b2";
+
     quadrato.classList.add("backyellow");
   }
-
   console.log(i);
 
-  quadrato.addEventListener("mouseover", function () {
+  quadrato.addEventListener("click", function () {
+    // quadrato.classList.add("backred");
     quadrato.style.backgroundColor = "red";
   });
 
-  quadrato.addEventListener("mouseleave", function () {
-    quadrato.style.backgroundColor = "initial";
+  quadrato.addEventListener("dblclick", function () {
+    // quadrato.classList.remove("backred");
+    quadrato.style.backgroundColor = "white";
   });
+
+  /*
+    non riesco a far andare riga 32 e 37...
+  */
 }
 
 /*
